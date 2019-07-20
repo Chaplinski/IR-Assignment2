ed42ff1e61e509892470b53d1e38549e126e4f4e
Scott <scott.chaplinski@gmail.com>
2019-07-20 18:37:14 -0500

    working commit

--- index.py
+++ index.py
@@ -7,8 +7,12 @@ import sys
 #import other modules as needed
 
 class Index:
-    def __init__(self, path):
+    def __init__(self, path, index, query, total_number_of_documents, stop_words):
         self.path = path
+        self.index = index
+        self.query = query
+        self.total_number_of_documents = total_number_of_documents
+        self.stop_words = stop_words
 
     def buildIndex(self):
         #function to read documents from collection, tokenize and build the index with tokens
@@ -22,7 +26,9 @@ class Index:
         # text_files = ['Text-001.txt', 'Text-002.txt']
         text_dictionary = {}
         text_id = 1
-        total_number_of_documents = len(text_files)
+        self.total_number_of_documents = len(text_files)
+        # list of stop words
+        stop_words_list = self.stop_words
 
         for text_file in text_files:
             # concatenate path with file name
@@ -35,21 +41,56 @@ class Index:
             text_contents = self.convert_string_to_list(text_contents)
 
             # build dictionary
-            text_dictionary = self.build_dictionary(text_id, text_contents, text_dictionary)
+            text_dictionary = self.build_dictionary(text_id, text_contents, text_dictionary, stop_words_list)
 
             text_id += 1
 
         # calculate the idf of each term
-        self.calculate_idf(text_dictionary, total_number_of_documents)
+        self.calculate_idf(text_dictionary, self.total_number_of_documents)
         # end timer
         end = time.clock()
         total_time = end - start
-        return text_dictionary  #, total_time
+        self.index= text_dictionary
+        # return text_dictionary  #, total_time
 
-    # def exact_query(self, query_terms, k):
+    def exact_query(self, query_terms, k):
 	# #function for exact top K retrieval (method 1)
 	# #Returns at the minimum the document names of the top K documents ordered in decreasing order of similarity score
-	#
+        print('query: ', query_terms)
+        print('top k: ', k)
+        print('index: ', self.index)
+
+        query_tf_idf_dict = {}
+        index_tf_idf_dict = {}
+        # for each word in the query
+        for word, list in query_terms.items():
+            # if that word is not a stop word
+            if word not in self.stop_words:
+                # get tf-idf of this query term
+                query_term_tf_idf = list[0] * list[1]
+                query_tf_idf_dict[word] = query_term_tf_idf
+                # print('list:', list, 'tf-idf:', query_term_tf_idf)
+                # if the word appears in self.index
+                if word in self.index:
+                    # get idf from the dictionary
+                    dictionary_idf = self.index[word][0]
+                    print(word, 'is in the index and its idf value is:', dictionary_idf)
+
+                    for doc_id_list in self.index[word]:
+                        # skip the first list item since it is the idf
+                        if doc_id_list != self.index[word][0]:
+
+                            doc_id = doc_id_list[0]
+                            word_tf = doc_id_list[1]
+                            doc_word_tf_idf = word_tf * dictionary_idf
+                            if index_tf_idf_dict[doc_id] in index_tf_idf_dict:
+                                index_tf_idf_dict[doc_id] = {}
+                                index_tf_idf_dict[doc_id][word] = doc_word_tf_idf
+                                print('doc id is:', doc_id, 'and tf-idf is:', doc_word_tf_idf)
+                                # sys.exit()
+        print(index_tf_idf_dict)
+
+
 	# def inexact_query_champion(self, query_terms, k):
 	# #function for exact top K retrieval using champion list (method 2)
 	# #Returns at the minimum the document names of the top K documents ordered in decreasing order of similarity score
@@ -94,7 +135,7 @@ class Index:
 
         return contents
 
-    def build_dictionary(self, text_id, word_list, text_dictionary):
+    def build_dictionary(self, text_id, word_list, text_dictionary, stop_words):
 
         this_dict = text_dictionary
         total_words_in_document = len(word_list)
@@ -102,47 +143,49 @@ class Index:
         integer = 0
         # for every word in a text
         for potential_new_word in word_list:
-            # check if the word already exists in the dictionary
-            if potential_new_word in this_dict:
-                # if word does exist then access its value array
-                for key, value in this_dict.items():
-                    # if the new word being added is the same as a key value
-                    if key == potential_new_word:
-                        # temp list holds values of all texts that already have int values saved
-                        already_saved_texts = []
-                        for list in value:
-                            # append text name to already_saved_texts list
-                            already_saved_texts.append(list[0])
-                        # set Boolean to false as a default
-                        is_already_saved = False
-                        # iterate though already_saved_texts looking for current text
-                        for text_val in already_saved_texts:
-                            # if current text exists set Boolean to true
-                            if text_val == text_id:
-                                is_already_saved = True
-
-                        # if text already has values saved
-                        if is_already_saved:
-                            # iterate through lists
+            # make sure stop word is not in list
+            if potential_new_word not in stop_words:
+                # check if the word already exists in the dictionary
+                if potential_new_word in this_dict:
+                    # if word does exist then access its value array
+                    for key, value in this_dict.items():
+                        # if the new word being added is the same as a key value
+                        if key == potential_new_word:
+                            # temp list holds values of all texts that already have int values saved
+                            already_saved_texts = []
                             for list in value:
-                                # position 0 in a list refers to the definition of what text it refers to
-                                if list[0] == text_id:
-                                    # if first value in key list is a text that already has this term then append
-                                    # word int location to list
-                                    list[1].append(integer)
-                        # if list does not already have values saved
-                        else:
-                            # append text and word int location to list
-                            value.append([text_id, [integer]])
+                                # append text name to already_saved_texts list
+                                already_saved_texts.append(list[0])
+                            # set Boolean to false as a default
+                            is_already_saved = False
+                            # iterate though already_saved_texts looking for current text
+                            for text_val in already_saved_texts:
+                                # if current text exists set Boolean to true
+                                if text_val == text_id:
+                                    is_already_saved = True
 
-            else:
-                # if word does not already exist in dictionary
-                # create new list containing text ID and int position in text to become value of new key
-                new_list = [text_id, [integer]]
-                # update dictionary to hold new key/value
-                this_dict.update({potential_new_word: [new_list]})
+                            # if text already has values saved
+                            if is_already_saved:
+                                # iterate through lists
+                                for list in value:
+                                    # position 0 in a list refers to the definition of what text it refers to
+                                    if list[0] == text_id:
+                                        # if first value in key list is a text that already has this term then append
+                                        # word int location to list
+                                        list[1].append(integer)
+                            # if list does not already have values saved
+                            else:
+                                # append text and word int location to list
+                                value.append([text_id, [integer]])
 
-            integer += 1
+                else:
+                    # if word does not already exist in dictionary
+                    # create new list containing text ID and int position in text to become value of new key
+                    new_list = [text_id, [integer]]
+                    # update dictionary to hold new key/value
+                    this_dict.update({potential_new_word: [new_list]})
+
+                integer += 1
 
         this_dict = self.calculate_tf(this_dict, total_words_in_document, text_id)
 
@@ -158,10 +201,16 @@ class Index:
                     # measure the number of times a word appears in a doc
                     number_of_appearances_in_doc = len(item[1])
                     # divide it by the word count of the entire document
-                    tf = number_of_appearances_in_doc/total_words_in_document
+                    tf = number_of_appearances_in_doc
                     # print(tf)
                     # calculate w
                     w = (1 + math.log10(tf))
+                    # print('word:', key)
+                    # print('doc appearances: ', number_of_appearances_in_doc)
+                    # print('doc words total: ', total_words_in_document)
+                    # print('tf: ', tf)
+                    # print('w:', w)
+                    # sys.exit()
                     # print(w)
                     # sys.exit()
                     # insert into list
@@ -175,10 +224,18 @@ class Index:
         for key, value in this_dict.items():
             # calculate IDF
             idf = math.log10(total_number_of_documents/len(value))
+            # print(this_dict)
+            # sys.exit()
             # add IDF to list in the first position
             value.insert(0, idf)
 
         return this_dict
+
+    def get_stop_words(self):
+        f = open('stop-list/stop-list.txt', "r")
+        contents = f.read()
+        contents = self.convert_string_to_list(contents)
+        self.stop_words = contents
 
     def create_query_sentence(self, query_terms):
         # create sentence describing the query
@@ -210,10 +267,61 @@ class Index:
             i += 1
         return doc_dict
 
+    # get query and store terms as a list
+    def ask_for_query(self, final_index):
+        self.query = input("Enter your query: ")
+        self.query = self.convert_string_to_list(self.query)
+        # create dictionary to keep track of word occurrences in query
+        this_dict = {}
+        # for each word in query
+        for item in self.query:
+            # if the word is not in the dictionary add it with a value of 1 occurrence
+            if item not in this_dict:
+                this_dict.update({item: 1})
+            else:
+                number_of_occurrences = this_dict.get(item)
+                number_of_occurrences += 1
+                this_dict.update({item: number_of_occurrences})
 
-index = Index('collection3/')
-final_index = index.buildIndex()
-print(final_index)
+        total_words_in_query = len(self.query)
+        # print(total_words_in_query)
+        # for each word in the query, calculate the tf-idf
+        for key, value in this_dict.items():
+            # calculate tf
+            number_of_appearances_in_query = value
+            # divide it by the word count of the entire query
+            tf = number_of_appearances_in_query
+            # calculate w
+            w = (1 + math.log10(tf))
+
+            idf = ''
+            # get idf that is already stored in inverted index
+            if key in final_index:
+                idf = final_index[key][0]
+
+            # store w and idf as value for word key in this dict
+            this_dict[key] = [w, idf]
+
+        return this_dict
+
+    def show_query(self):
+       print('')
+       # print(self.query)
+
+
+# instantiate the object
+index = Index('collection3/', '', '', '', '')
+# build the index, which is stored as self.index
+index.buildIndex()
+# get the stop words list
+# index.get_stop_words()
+# ask for a query and store it as query
+query = index.ask_for_query(index.index)
+# function for exact top K retrieval (method 1)
+index.exact_query(query, 10)
+
+# stop_words = index.get_stop_words()
+# print(final_index)
 # print('Index built in', final_index[1], 'seconds')
 #
 # index.and_query(['with', 'had', 'the', 'was'])
